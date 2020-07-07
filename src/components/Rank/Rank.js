2255a14a31bd3da03cbf7d2ab8b42cc6894d6b53
Susiepolly <susie_poll@yahoo.co.uk>
2020-07-07 18:37:47 +0100

    basic layout

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rank = () => {
+  return(
+    <div>
+      <p className='white f3'>{'Susie, your current rank is...'}</p>
+      <p className='white f1'>{'#5'}</p>
+    </div>
+  );
+}
+
+export default Rank;
